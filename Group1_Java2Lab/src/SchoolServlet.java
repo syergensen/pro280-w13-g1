393fdae6ca75aaeac04882406f54442f7f2a1f2d
acottrill <acottrill@student.neumont.edu>
2013-02-28 18:02:51 -0700

    drew's second commit added school.jsp

---
+++ Group1_Java2Lab/src/SchoolServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: acottrill
+ * Date: 2/27/13
+ * Time: 6:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class SchoolServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
