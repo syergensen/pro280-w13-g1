b14055e655b0ea01771dd0c7f50efbf50c260ac8
JohnR31415 <JohnR31415@gmail.com>
2013-03-06 17:41:21 -0700

    Added the init params for the ResultCalculator class

--- Group1_Java2Lab/src/controller/ResultCalculator.java
+++ Group1_Java2Lab/src/controller/ResultCalculator.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,11 @@ import java.io.IOException;
  * Time: 7:17 PM
  * To change this template use File | Settings | File Templates.
  */
-@WebServlet(name = "ResultCalculator", urlPatterns = "/result/calculate")
+@WebServlet(name = "ResultCalculator", urlPatterns = {"/result/calculate"},
+        initParams =
+                {
+                        @WebInitParam(name = "success", value = "/results.jsp"),
+                        @WebInitParam(name = "failure", value = "/aspirations.jsp")})
 public class ResultCalculator extends HttpServlet
 {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
@@ -22,12 +27,28 @@ public class ResultCalculator extends HttpServlet
         String quarter = request.getParameter("quarter");
         Integer year = Integer.parseInt(request.getParameter("year"));
         String degree = request.getParameter("degree");
-        Integer extraquarterfull = Integer.parseInt(request.getParameter("extra-quarter-full"));
-        Integer extraquarterpart = Integer.parseInt(request.getParameter("extra-quarter-part"));
-        Integer loanpercent = Integer.parseInt(request.getParameter("loan-percent"));
-        Integer debt1 = Integer.parseInt(request.getParameter("debt1"));
-        Integer debt2 = Integer.parseInt(request.getParameter("debt2"));
-        Integer debt3 = Integer.parseInt(request.getParameter("debt3"));
+        Float extraquarterfull = Float.valueOf(request.getParameter("extra-quarter-full"));
+        Float extraquarterpart = Float.valueOf(request.getParameter("extra-quarter-part"));
+        Float loanpercent = Float.valueOf(request.getParameter("loan-percent"));
+        Float debt1 = Float.valueOf(request.getParameter("debt1"));
+        Float debt2 = Float.valueOf(request.getParameter("debt2"));
+        Float debt3 = Float.valueOf(request.getParameter("debt3"));
+
+        //calculates the result
+
+        request.getRequestDispatcher(getServletConfig().getInitParameter("success")).forward(request, response);
+//        Salary..................${salary}<br>
+//            Income Tax..............${income}<br>
+//            Health Insurance........${healthinsurance}<br>
+//            Misc. Insurance.........${miscinsurance}<br>
+//            Car Payment.............${carpayment}<br>
+//            Mortgage/Rent...........${mortgage}<br>
+//            Utilities...............${utilities}<br>
+//            Total Loans.............${totalloans}<br>
+//            In-School Savings.......${inschoolsavings}<br>
+//        <br>
+//        <br>
+//            Discretionary Income: ${discretionary}
 
     }
 
