cd686915bc08eea7297bdc52cb7f549fe9c5b948
JohnR31415 <JohnR31415@gmail.com>
2013-03-06 17:57:45 -0700

    Added the variables that will be sent to result.jsp, also added them to the session's attributes. At the end it forwards to result.jsp.

--- Group1_Java2Lab/src/controller/ResultCalculator.java
+++ Group1_Java2Lab/src/controller/ResultCalculator.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -24,17 +25,37 @@ public class ResultCalculator extends HttpServlet
 {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
-        String quarter = request.getParameter("quarter");
-        Integer year = Integer.parseInt(request.getParameter("year"));
-        String degree = request.getParameter("degree");
-        Float extraquarterfull = Float.valueOf(request.getParameter("extra-quarter-full"));
-        Float extraquarterpart = Float.valueOf(request.getParameter("extra-quarter-part"));
-        Float loanpercent = Float.valueOf(request.getParameter("loan-percent"));
-        Float debt1 = Float.valueOf(request.getParameter("debt1"));
-        Float debt2 = Float.valueOf(request.getParameter("debt2"));
-        Float debt3 = Float.valueOf(request.getParameter("debt3"));
+        HttpSession session = request.getSession();
+        String quarter = (String)session.getAttribute("quarter");
+        Integer year = Integer.parseInt((String)session.getAttribute("year"));
+        String degree = (String)session.getAttribute("degree");
+        Double extraquarterfull = Double.parseDouble((String)session.getAttribute("extra-quarter-full"));
+        Double extraquarterpart = Double.parseDouble((String)session.getAttribute("extra-quarter-part"));
+        Double loanpercent = Double.parseDouble((String)session.getAttribute("loan-percent"));
+        Double debt1 = Double.parseDouble((String)session.getAttribute("debt1"));
+        Double debt2 = Double.parseDouble((String)session.getAttribute("debt2"));
+        Double debt3 = Double.parseDouble((String)session.getAttribute("debt3"));
 
         //calculates the result
+        Double income = 0.00;
+        Double healthinsurance = 0.00;
+        Double miscinsurance = 0.00;
+        Double carpayment = 0.00;
+        Double mortgage = 0.00;
+        Double utilities = 0.00;
+        Double totalloans = 0.00;
+        Double inschoolsavings = 0.00;
+        Double discretionary = 0.00;
+
+        session.setAttribute("income", income);
+        session.setAttribute("healthinsurance", healthinsurance);
+        session.setAttribute("miscinsurance", miscinsurance);
+        session.setAttribute("carpayment", carpayment);
+        session.setAttribute("mortgage", mortgage);
+        session.setAttribute("utilities", utilities);
+        session.setAttribute("totalloans", totalloans);
+        session.setAttribute("inschoolsavings", inschoolsavings);
+        session.setAttribute("discretionary", discretionary);
 
         request.getRequestDispatcher(getServletConfig().getInitParameter("success")).forward(request, response);
 //        Salary..................${salary}<br>
