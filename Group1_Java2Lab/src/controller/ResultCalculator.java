211b286c49aa7ca158eeea587c10aab95979ab8b
Samantha Gomez <samanthag92@gmail.com>
2013-03-12 15:58:19 -0600

    Commented out the results page and fixed the link in the ResultCalculator class to point to the results.jsp page inside the WEB-INF folder.

--- Group1_Java2Lab/src/controller/ResultCalculator.java
+++ Group1_Java2Lab/src/controller/ResultCalculator.java
@@ -16,8 +16,8 @@ import java.io.IOException;
 @WebServlet(name = "ResultCalculator", urlPatterns = {"/result/calculate"},
         initParams =
                 {
-                        @WebInitParam(name = "success", value = "/results.jsp"),
-                        @WebInitParam(name = "failure", value = "/aspirations.jsp")})
+                        @WebInitParam(name = "success", value = "/WEB-INF/results.jsp"),
+                        @WebInitParam(name = "failure", value = "/WEB-INF/aspirations.jsp")})
 public class ResultCalculator extends HttpServlet
 {
     @EJB
@@ -51,32 +51,32 @@ public class ResultCalculator extends HttpServlet
         HttpSession session = request.getSession();
 
         //School
-        String startMonth = (String)session.getAttribute("quarter");
-        Integer startYear = Integer.parseInt((String)session.getAttribute("year"));
-        Degree degree = degreeManager.findDegree((Degree)session.getAttribute("degree"));
-        Integer expectedFull = Integer.parseInt((String)session.getAttribute("extra-quarter-full"));
-        Integer expectedPart = Integer.parseInt((String)session.getAttribute("extra-quarter-part"));
-        Integer loanPercent = Integer.parseInt((String)session.getAttribute("loan-percent"));
-        Double scholarships = Double.parseDouble((String)session.getAttribute("scholarships"));
-        Integer interestRate = Integer.parseInt((String)session.getAttribute("interestrate"));
-        Double debt1 = Double.parseDouble((String)session.getAttribute("debt1"));
-        Double debt2 = Double.parseDouble((String)session.getAttribute("debt2"));
-        Double debt3 = Double.parseDouble((String)session.getAttribute("debt3"));
+//        String startMonth = (String)session.getAttribute("quarter");
+//        Integer startYear = Integer.parseInt((String)session.getAttribute("year"));
+////        Degree degree = degreeManager.findDegree((Degree)session.getAttribute("degree"));
+//        Integer expectedFull = Integer.parseInt((String)session.getAttribute("extra-quarter-full"));
+//        Integer expectedPart = Integer.parseInt((String)session.getAttribute("extra-quarter-part"));
+//        Integer loanPercent = Integer.parseInt((String)session.getAttribute("loan-percent"));
+//        Double scholarships = Double.parseDouble((String)session.getAttribute("scholarships"));
+//        Integer interestRate = Integer.parseInt((String)session.getAttribute("interestrate"));
+//        Double debt1 = Double.parseDouble((String)session.getAttribute("debt1"));
+//        Double debt2 = Double.parseDouble((String)session.getAttribute("debt2"));
+//        Double debt3 = Double.parseDouble((String)session.getAttribute("debt3"));
 
         //Housing & Lifestyle
-        Double rent = Double.parseDouble((String)session.getAttribute("rent"));
-        Double utilities = Double.parseDouble((String)session.getAttribute("utilities"));
-        Double lunch = Double.parseDouble((String)session.getAttribute("go_out_to_lunch"));
-        Double dinner = Double.parseDouble((String)session.getAttribute("go_out_to_dinner"));
-        Double entertainment = Double.parseDouble((String)session.getAttribute("spend_on_entertainment"));
+//        Double rent = Double.parseDouble((String)session.getAttribute("rent"));
+//        Double utilities = Double.parseDouble((String)session.getAttribute("utilities"));
+//        Double lunch = Double.parseDouble((String)session.getAttribute("go_out_to_lunch"));
+//        Double dinner = Double.parseDouble((String)session.getAttribute("go_out_to_dinner"));
+//        Double entertainment = Double.parseDouble((String)session.getAttribute("spend_on_entertainment"));
+//
+//        //Post-Graduation
+//        Region region = (Region)session.getAttribute("region");
+//        Car car = (Car)session.getAttribute("car");
+//        Housing housing = (Housing)session.getAttribute("housing");
+//        Double payments = Double.parseDouble((String)session.getAttribute("payments"));
 
-        //Post-Graduation
-        Region region = (Region)session.getAttribute("region");
-        Car car = (Car)session.getAttribute("car");
-        Housing housing = (Housing)session.getAttribute("housing");
-        Double payments = Double.parseDouble((String)session.getAttribute("payments"));
-
-        DegreeRegionSalary drs = degreeRegionSalaryManager.getDegreeRegionSalary(degree.getId(), region.getId());
+//        DegreeRegionSalary drs = degreeRegionSalaryManager.getDegreeRegionSalary(degree.getId(), region.getId());
 
         Double salary;
         Double studentloan;
@@ -89,44 +89,42 @@ public class ResultCalculator extends HttpServlet
         Double discretionary;
 
         //Salary
-        salary = drs.getSalary() / 12;
+//        salary = drs.getSalary() / 12;
 
         //Student loan
-        if(loanPercent == 0)
-        {
-            studentloan = 0.0;
-        }
-        else
-        {
-            Double totalprice = degree.getDuration() * 7200.0;
-            Double totalrent = rent / 4.34812 * 100;
-            Double totalutilities = utilities / 4.34812 * 100;
-            totalprice += (totalrent + totalutilities);
-            Double totalloan = totalprice * (loanPercent / 100);
-            Double annualrate = interestRate / 1200.0;
-            Double payment = annualrate + (annualrate / (Math.pow(annualrate + 1, 120) - 1)) * totalloan;
-            studentloan = payment;
-        }
+//        if(loanPercent == 0)
+//        {
+//            studentloan = 0.0;
+//        }
+//        else
+//        {
+//            Double totalprice = degree.getDuration() * 7200.0;
+//            Double totalrent = rent / 4.34812 * 100;
+//            Double totalutilities = utilities / 4.34812 * 100;
+//            totalprice += (totalrent + totalutilities);
+//            Double totalloan = totalprice * (loanPercent / 100);
+//            Double annualrate = interestRate / 1200.0;
+//            Double payment = annualrate + (annualrate / (Math.pow(annualrate + 1, 120) - 1)) * totalloan;
+//            studentloan = payment;
+//        }
 
         //Income tax
-        Double firstBracket = 9000 * 0.1;
-        Double secondBracket = (36000 - 9000) * 0.15;
-        Double thirdBracket = (drs.getSalary() - 36000) * .25;
-        incometax = firstBracket + secondBracket + thirdBracket;
-        incometax = incometax / 12;
+//        Double firstBracket = 9000 * 0.1;
+//        Double secondBracket = (36000 - 9000) * 0.15;
+//        Double thirdBracket = (drs.getSalary() - 36000) * .25;
+//        incometax = firstBracket + secondBracket + thirdBracket;
+//        incometax = incometax / 12;
 
         // Misc Expenses
-        Double lunchMonth = lunch * 4.34812;
-        Double dinnerMonth = dinner * 4.34812;
-        miscexpenses = lunchMonth + dinnerMonth + entertainment;
+//        Double lunchMonth = lunch * 4.34812;\
 
         //Car Expenses
-        Double mpg = car.getMpg();
-        Double gasMonth = 1600 / mpg * 3.6;
-        carexpenses = gasMonth + payments;
+//        Double mpg = car.getMpg();
+//        Double gasMonth = 1600 / mpg * 3.6;
+//        carexpenses = gasMonth + payments;
 
         //Mortgage/Rent
-        mortgage = housing.getRent() + housing.getUtilities();
+//        mortgage = housing.getRent() + housing.getUtilities();
 
         //Misc. Loans
         //to be determined
@@ -136,17 +134,17 @@ public class ResultCalculator extends HttpServlet
         //to be determined
         savings = 0.0;
 
-        discretionary = salary - (studentloan + incometax + miscexpenses + carexpenses + mortgage + miscloans + savings);
+//        discretionary = salary - (studentloan + incometax + miscexpenses + carexpenses + mortgage + miscloans + savings);
 
-        session.setAttribute("salary", salary);
-        session.setAttribute("studentloan", studentloan);
-        session.setAttribute("incometax", incometax);
-        session.setAttribute("miscexpenses", miscexpenses);
-        session.setAttribute("carexpenses", carexpenses);
-        session.setAttribute("mortgage", mortgage);
-        session.setAttribute("miscloans", miscloans);
-        session.setAttribute("savings", savings);
-        session.setAttribute("discretionary", discretionary);
+//        session.setAttribute("salary", salary);
+//        session.setAttribute("studentloan", studentloan);
+//        session.setAttribute("incometax", incometax);
+//        session.setAttribute("miscexpenses", miscexpenses);
+//        session.setAttribute("carexpenses", carexpenses);
+//        session.setAttribute("mortgage", mortgage);
+//        session.setAttribute("miscloans", miscloans);
+//        session.setAttribute("savings", savings);
+//        session.setAttribute("discretionary", discretionary);
 
 
 
